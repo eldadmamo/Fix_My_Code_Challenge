3669af132bd78d9f479abe229e5732a61ac07d28
Eldad Fikre <71949682+eldadmamo@users.noreply.github.com>
2024-03-24 20:27:39 +0300

    Update 0-fizzbuzz.py

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -5,23 +5,25 @@ import sys
 
 
 def fizzbuzz(n):
-        """
-            FizzBuzz function prints numbers from 1 to n separated by a space.
-            """
-            if n < 1:
-                return
+    """
+    FizzBuzz function prints numbers from 1 to n 
 
-tmp_result = []
-for i in range(1, n + 1):
-    if (i % 3) == 0 and (i % 5) == 0:
-        tmp_result.append("FizzBuzz")
-    elif (i % 3) == 0:
-        tmp_result.append("Fizz")
-    elif (i % 5) == 0:
-        tmp_result.append("Buzz")
-    else:
-        tmp_result.append(str(i))
-        print(" ".join(tmp_result))
+    - For multiples of three print
+    """
+    if n < 1:
+        return
+
+    tmp_result = []
+    for i in range(1, n + 1):
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
 
 
 if __name__ == '__main__':
@@ -31,5 +33,5 @@ if __name__ == '__main__':
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
-number = int(sys.argv[1])
-fizzbuzz(number)
+    number = int(sys.argv[1])
+    fizzbuzz(number)
